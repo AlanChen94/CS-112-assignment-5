e834d5d4cc9de86414bf674783dd94f9178cec5b
MyNameIsDhaval <dhavalp.00@gmail.com>
2013-04-29 17:52:52 -0300

    Update Friends.java

--- Friends.java
+++ Friends.java
@@ -1,9 +1,24 @@
 // Alan Chen & Dhaval Patel
+import java.util.Scanner;
+
 public class Friends{
 
     public static void main(String[] args)
     {
         
     }
-  
+    
+    public void initInterface()
+    {
+
+        System.out.print("Please enter the name of the graph file : ")
+        try 
+        {
+            Scanner s = new Scanner();    
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Error: File not found");
+        }
+    }
 }
