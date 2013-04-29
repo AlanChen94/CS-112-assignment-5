24e4ed4971d7c9d13473985ff15ece8be1134ba0
AlanChen94 <imalandonthurtme@yahoo.com>
2013-04-29 18:40:41 -0300

    Create Vertices.java

---
+++ Vertices.java
@@ -0,0 +1,17 @@
+public class Vertices {
+  
+	String name;
+	char school;
+	String schoolname;
+	Vertices next;
+	
+	public Vertices (String name, char school, String schoolname){
+		
+		this.name = name;
+		this.school = school;
+		this.schoolname = schoolname;
+		this.next = null;
+		
+	}
+	
+}
