6edf35c0eeb1fd00e8974e7a8f43cb009aef89cd
MyNameIsDhaval <dhavalp.00@gmail.com>
2013-05-01 17:16:43 -0400

    Updated Friends.java

--- Friends.java
+++ Friends.java
@@ -5,20 +5,40 @@ public class Friends{
 
     public static void main(String[] args)
     {
-        
+        initGraph();
+
+        Scanner s = new Scanner(System.in);
+
+        while(true)
+        {
+            menu();
+
+
+            System.output.println("Do you want to continue? y/n");
+
+        }
     }
     
-    public void initInterface()
+    public void initGraph()
     {
-
+        String graph = s.nextString();
         System.out.print("Please enter the name of the graph file : ")
         try 
         {
-            Scanner s = new Scanner();    
+            Scanner s = new Scanner(System.in);   
+            graph = s.nextString();
         }
         catch (FileNotFoundException e)
         {
             System.out.println("Error: File not found");
         }
+        Graph g = new Graph(graph);
     }
+
+    public void menu()
+    {
+
+    }
+
+
 }
