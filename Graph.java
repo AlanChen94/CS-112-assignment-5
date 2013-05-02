2600a4f34a23a37dfb60fc715535f18f15942562
AlanChen94 <imalandonthurtme@yahoo.com>
2013-05-02 03:18:22 -0300

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -16,21 +16,27 @@ public class Graph {
 	public Graph(String graphFile){
 		graph = new ArrayList<Vertices>();
 		access = new HashMap<String, Integer>();
-		buildGraph(graphFile);
+		try {
+			buildGraph(graphFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void buildGraph(String docsFile) throws FileNotFoundException{
 		Scanner sc = new Scanner(new File(docsFile));
 		int counter = Integer.parseInt(sc.nextLine());
 		while(counter != 0){
-			String text = sc.next();
+			String text = sc.nextLine();
 			Vertices person;
-			if(text.indexOf('|') + 1 == 'n'){
+			if(text.charAt(text.indexOf('|') + 1) == 'n'){
 				person = new Vertices(text.substring(0, text.indexOf('|')), 'n', "");
 			}else
 				person = new Vertices(text.substring(0, text.indexOf('|')), 'y', text.substring(text.indexOf('y')+2));
 			graph.add(person);
+			System.out.println(person);
 			access.put(person.name, graph.indexOf(person));
+			counter--;
 		}while(sc.hasNext()){
 			String text = sc.next();
 			String name1 = text.substring(0, text.indexOf('|'));
@@ -38,9 +44,11 @@ public class Graph {
 			int index1 = access.get(name1);
 			int index2 = access.get(name2);
  			Vertices temp1 = graph.get(index1);
+ 			graph.get(index2).next = temp1;
  			Vertices temp2 = graph.get(index2);
-			temp1.next = graph.get(index2);
-			temp2.next = graph.get(index1);
+ 			temp2.next = null;
+ 			graph.get(index1).next = temp2;
 		}
 	}
 }
+
