62343ad0bfb3bfe2e73fcb3b8d5678bdd33d27a8
AlanChen94 <imalandonthurtme@yahoo.com>
2013-05-06 02:16:16 -0300

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -16,24 +16,15 @@ public class Graph {
 	public Graph(String graphFile){
 		graph = new ArrayList<Vertices>();
 		access = new HashMap<String, Integer>();
-		buildGraph(graphFile);		
-	}
-	
-	public Graph(){
-		graph = new ArrayList<Vertices>();
-		access = new HashMap<String, Integer>();	
-	}
-	
-	public void buildGraph(String docsFile){
-		Scanner sc = null;
-		try
-		{
-			sc = new Scanner(new File(docsFile));
-		}catch (FileNotFoundException e)
-		{
-			System.out.println("File not found!");
-			System.exit(0);
+		try {
+			buildGraph(graphFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	public void buildGraph(String docsFile) throws FileNotFoundException{
+		Scanner sc = new Scanner(new File(docsFile));
 		int counter = Integer.parseInt(sc.nextLine());
 		while(counter != 0){
 			String text = sc.nextLine();
@@ -43,9 +34,7 @@ public class Graph {
 			}else
 				person = new Vertices(text.substring(0, text.indexOf('|')), 'y', text.substring(text.indexOf('y')+2));
 			graph.add(person);
-
-			// Output names -- For debugging
-			//System.out.println(person);
+			System.out.println(person);
 			access.put(person.name, graph.indexOf(person));
 			counter--;
 		}while(sc.hasNext()){
@@ -55,12 +44,41 @@ public class Graph {
 			int index1 = access.get(name1);
 			int index2 = access.get(name2);
  			Vertices temp1 = graph.get(index1);
- 			graph.get(index2).next = temp1;
  			Vertices temp2 = graph.get(index2);
+ 			temp1.next = null;
+ 			graph.get(index2).next = temp1;
  			temp2.next = null;
  			graph.get(index1).next = temp2;
 		}
 	}
+	
+	public ArrayList<Vertices> subGraph(String schoolname){
+		if(schoolname.isEmpty()){
+			return null;
+		}ArrayList<Vertices> SubGraph = new ArrayList<Vertices>();
+		for(int i = 0; i < graph.size(); i++){
+			if(graph.get(i).schoolname == schoolname){
+				Vertices temp = graph.get(i);
+				Vertices temp1 = graph.get(i);
+				temp.next = null;
+				SubGraph.add(temp);
+				while(temp1.next != null){
+					if(temp1.next.schoolname == schoolname){
+						Vertices tempNeighbor = temp1.next;
+						tempNeighbor.next = null;
+						SubGraph.get(access.get(temp)).next = tempNeighbor;
+					}temp1 = temp1.next;	
+				}
+			}
+		}return SubGraph;
+	}
+	
+	public String shortestPath(String name1, String name2){
+		
+		return null;
+	}
+}
+
 	
 	public static Graph createSubGraph(Graph g, String school)
 	{
